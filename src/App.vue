fce93a2597af241be09e1adf959a83d9e4cc36bd
Samuel Campelo <>
2025-04-12 12:34:15 -0300

    refactor: enhance client registration form and improve client display logic

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,25 @@
 <template>
   <div id="app">
-    <Cliente :cliente="clienteBruno" />
-    <Cliente :cliente="clienteBruno" />
-    <Cliente :cliente="clienteBruno" />
+    
+    <h3>Cadastro: </h3>
+    <small id="cadastroErro" v-show="deuErro">Preencha todos os campos!</small><br>
+    <input type="text" placeholder="nome" v-model="nomeField"> <br>
+    <input type="email" placeholder="email" v-model="emailField"> <br>
+    <input type="number" placeholder="idade" v-model="idadeField"> <br>
+    <button @click="adicionarCliente">Adicionar</button> <hr>
+    
+    <div>
+     
+      <div v-for="(cliente, index) in cliente" :key="cliente.id">
+        <h4>{{ index +1 }}</h4>
+          <Cliente :cliente="cliente" @deletarClienteEvent="deletarCliente($event)"/>
+        <hr>
+        
+
+      </div>
+
+    </div>
+
   </div>
 </template>
  
@@ -10,18 +27,80 @@
 import Cliente from "./components/Cliente";
 
 export default {
+  deuErro: false,
   components: { Cliente },
   data() {
     return {
-      clienteBruno: {
-        nome: "Bruno Silva",
-        email: "bruno@email.com",
-        idade: 32
-      }
+      nomeField: "",
+      emailField: "",
+      idadeField: 0,
+      cliente: [
+        {
+          id: 1,
+          nome: "Marcio Silva",
+          email: "marcio@email.com",
+          idade: 52
+        },
+        {
+          id: 2,
+          nome: "Lucas Silva",
+          email: "lucas@email.com",
+          idade: 12
+        },
+        {
+          id: 3,
+          nome: "José Silva",
+          email: "jose@email.com",
+          idade: 42
+        },
+        {
+          id: 4,
+          nome: "Ruan Silva",
+          email: "ruan@email.com",
+          idade: 22
+        },
+        {
+          id: 5,
+          nome: "Bruno Silva",
+          email: "bruno@email.com",
+          idade: 32
+        }
+      ]
     };
-  }
+  },
+  methods: {
+        adicionarCliente() {
+          if (this.nomeField === "" || this.emailField === "" || this.idadeField === 0) {
+            alert("Preencha todos os campos!");
+            this.deuErro = true;
+          } else {
+           this.cliente.push({
+            nome: this.nomeField,
+            email: this.emailField,
+            idade: this.idadeField,
+            id: Date.now()
+          });
+          this.nomeField = "";
+          this.emailField = "";
+          this.idadeField = 0; 
+
+          this.deuErro = false;  
+          }
+        },
+        deletarCliente($event) {
+          console.log($event);
+          var id = $event.idCliente;
+          this.cliente = this.cliente.filter((cliente) => cliente.id != id);
+        }
+    }
 };
 </script>
 
-<style scoped>
+<style>
+
+ .cadastroErro{
+  color: red;
+  display: none;
+ }
+
 </style>
--- src/components/Cliente.vue
+++ src/components/Cliente.vue
@@ -1,28 +1,54 @@
 <template>
-  <div id="cliente">
+  <div :class="{'cliente': !isPremium,'cliente-premium': isPremium}">
     <h4>Nome: {{ cliente.nome }}</h4>
     <hr>
     <p>Email: {{ cliente.email }}</p>
-    <p>Idade: {{ cliente.idade }}</p>
+    <p v-if="showIdade === true">Idade: {{ cliente.idade }}</p>
+    <p v-else>O usuário escondeu a idade!</p>
+    <button @click="mudarCor">Mudar cor!</button>
+    <button @click="deletarCliente">Deletar</button> <hr>
+
   </div>
 </template>
 
 <script>
 export default {
+  data(){
+    return {
+      isPremium: false    
+    }
+  },
   props: {
     cliente: Object,
-    nome: String,
-    email: String,
-    idade: Number
-  }
+    showIdade: {
+      type: Boolean
+    }
+  },
+  methods: {
+    mudarCor(){
+      this.isPremium = !this.isPremium
+    },
+    deletarCliente(){
+      this.$emit("deletarClienteEvent", {component: this, idCliente: this.cliente.id})
+    }
+  },
 }
 </script>
 
 
 <style scoped>
-    #cliente{
-        background-color: rgb(223, 233, 241);
+    .cliente{
+        background-color: rgb(118, 192, 252);
         max-width: 600px;
+        height: 250px;
+        padding: 1%;
+        margin-top: 2%;
+    }
+
+    .cliente-premium{
+        background-color: rgb(0, 0, 0);
+        color: gold;
+        max-width:600px;
         height: 250px;
         padding: 1%;
         margin-top: 2%;
